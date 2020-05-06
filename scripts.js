7785c733b57e2f6a9f9f06881b61331e196fe390
MarcoDiSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-05-06 21:59:07 +0100

    gameboard module

--- scripts.js
+++ scripts.js
@@ -1,1 +1,34 @@
-//js
+//gameBoard Module
+var GameBoard = (function() {
+    let grid = [
+        ['1','2','3'],
+        ['4','5','6'],
+        ['7','8','9']
+    ];
+
+    const getGrid = () => {
+        return grid;
+    };
+
+    const setPlay = (row, column, marker) => {
+        grid[row][column] = marker;
+    }
+
+    const resetGrid = () => {
+        grid = grid.forEach(position => '-');
+    }
+    //public variables
+    return { getGrid, setPlay, resetGrid };
+})();
+
+//atm this works like GameBoard.getGrid()[1] for mid row and so on...
+
+
+//obj for players (factory)
+//controller  - factory
+
+//drawBoard func
+//gameLogic
+//scores
+
+//AI moves
