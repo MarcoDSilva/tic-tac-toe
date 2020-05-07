e695babc94d6d6f0ee5ce40994ca588430de6f01
MarcoDiSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-05-07 13:58:58 +0100

    drawing board

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
 //gameBoard Module
 const GameBoard = (function() {
     let grid = [
-        ['1','2','3'],
-        ['4','5','6'],
-        ['7','8','9']
-    ];
+        '1','2','3',
+        '4','5','6',
+        '7','8','9'];
 
     const getGrid = () => {
         return grid;
@@ -38,6 +37,18 @@ const Player = (name, mark) => {
     return { name, mark, addVictories, getVictories };
 }
 
+//DOM to get the cells and turning the elements onto an array
+const table = document.querySelector('.board');
+const tableGridsHTML = document.querySelectorAll('.cell');
+const cells = [...tableGridsHTML];
+
+function drawBoard() {
+    for (let i = 0; i < 9; i++) {
+        cells[i].innerHTML = GameBoard.getGrid()[i];
+    }
+}
+// cells.forEach(btn => btn.innerHTML = GameBoard.getGrid());
+
 //controller  - factory
 
 //drawBoard func
@@ -45,3 +56,5 @@ const Player = (name, mark) => {
 //scores
 
 //AI moves
+
+drawBoard();
